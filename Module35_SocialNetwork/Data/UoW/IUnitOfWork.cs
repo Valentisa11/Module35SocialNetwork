c5560c3aa0b650d985c3cc10948dcca8831284f7
Valery1103 <val.kobylina@gmail.com>
2023-10-17 00:09:00 +0500

    add Controllers, UoW

---
+++ Module35_SocialNetwork/Data/UoW/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Module35_SocialNetwork.Data.UoW
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int SaveChanges(bool ensureAutoHistory = false);
+
+        IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = true) where TEntity : class;
+    }
+}
